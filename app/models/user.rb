49a393d0a6da635db73bedcb632b1153aa23d1fa
鈴木佑輔 <oretoon@gmail.com>
2021-02-22 16:09:50 +0900

    model 追加　2

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,5 +44,5 @@ class User < ApplicationRecord
 
   before_save {self.email = email.downcase}
 
-  validates :name, {presence: true, length: maximum: 50},
+  validates :name, {presence: true, length: {maximum: 50}},
 end
